662e6a75eb3ecf0955b58510f2bc37364bf452e4
hasand <dimagmehanic@gmail.com>
2026-02-15 19:34:33 -0500

    add test and include functions

---
+++ R/include.R
@@ -0,0 +1,18 @@
+#' include function
+#'
+#' @param package library name
+#'
+#' @return library install and load
+#' @export
+#'
+#' @examples
+#' include("ggplot2")
+#' include("dplyr")
+
+include <- function(package) {
+  if (!require(package, character.only = TRUE)) {
+    install.packages(package, dependencies = TRUE)
+  }
+
+  library(package, character.only = TRUE)
+}
--- R/tmp.R
+++ R/tmp.R
@@ -1,4 +1,12 @@
-# test function
-test <- function() {
-  plot(cars)
+#' Test function plot cars dataset
+#'
+#' @param x dataset name
+#'
+#' @return A plot cars dataset
+#' @export
+#'
+#' @examples
+#' test()
+test <- function(x = cars) {
+  plot(x)
 }
