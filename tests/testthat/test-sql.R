00373b27965edb5d7c0f85e877be3379aa93dd95
hasand <dimagmehanic@gmail.com>
2026-06-23 15:47:44 -0400

    dir test

--- tests/testthat/test-sql.R
+++ tests/testthat/test-sql.R
@@ -1,9 +1,9 @@
-test_that("db fixture exists", {
+test_that("sql fixture exists", {
     db_path <- testthat::test_path("fixtures", "test.sql")
     expect_true(file.exists(db_path))
 })
 
-test_that("db fixture works", {
+test_that("sql fixture works", {
     db_path <- testthat::test_path("fixtures", "test.sql")
     result <- leer(db_path)
     expect_equal( result$leer("mtcars") |> dplyr::collect() |> nrow(), 32)
